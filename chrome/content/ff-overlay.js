26aa62ed9658828e3a2ed9ba4094e34727f61f7a
rickparrish <github@rick.parrish>
2014-05-30 20:26:53 -0400

    Add new labels to toolbar

--- chrome/content/ff-overlay.js
+++ chrome/content/ff-overlay.js
@@ -3,22 +3,17 @@ var foxsavvy = {
     // initialization code
     this.initialized = true;
     this.strings = document.getElementById("foxsavvy-strings");
+  },
+  
+  Refresh: function() {
+    document.getElementById('lblISP').value = "TODO ISP";
+    document.getElementById('lblCurrentUsage').value = "123.45 GB";
+    document.getElementById('lblPredictedUsage').value = "543.21 GB";
+    
+    setTimeout("foxsavvy.Refresh();", 30 * 60 * 1000); // 30 minutes
   }
 };
 
 window.addEventListener("load", function () { foxsavvy.onLoad(); }, false);
 
-
-foxsavvy.onFirefoxLoad = function(event) {
-  document.getElementById("contentAreaContextMenu")
-          .addEventListener("popupshowing", function (e) {
-    foxsavvy.showFirefoxContextMenu(e);
-  }, false);
-};
-
-foxsavvy.showFirefoxContextMenu = function(event) {
-  // show or hide the menuitem based on what the context menu is on
-  document.getElementById("context-foxsavvy").hidden = gContextMenu.onImage;
-};
-
-window.addEventListener("load", function () { foxsavvy.onFirefoxLoad(); }, false);
+setTimeout("foxsavvy.Refresh();", 1000);
