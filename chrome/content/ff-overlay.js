5742ccbbee1b4aa799dc22f9f4d2fcd54e73eb00
rickparrish <github@rick.parrish>
2014-05-30 20:07:23 -0400

    Provide proper labels
    Also remove some unneeded labels/code

--- chrome/content/ff-overlay.js
+++ chrome/content/ff-overlay.js
@@ -3,18 +3,6 @@ var foxsavvy = {
     // initialization code
     this.initialized = true;
     this.strings = document.getElementById("foxsavvy-strings");
-  },
-
-  onMenuItemCommand: function(e) {
-    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
-                                  .getService(Components.interfaces.nsIPromptService);
-    promptService.alert(window, this.strings.getString("helloMessageTitle"),
-                                this.strings.getString("helloMessage"));
-  },
-
-  onToolbarButtonCommand: function(e) {
-    // just reuse the function above.  you can change this, obviously!
-    foxsavvy.onMenuItemCommand(e);
   }
 };
 
